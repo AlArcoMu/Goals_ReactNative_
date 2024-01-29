3e76ab91373fd561b633dff28ae67ff9172e42f8
AlArcoMu <arconescaballero74@gmail.com>
2024-01-29 09:26:10 +0100

    GoalInputCommit

--- App.js
+++ App.js
@@ -1,100 +1,65 @@
-/**
- * @fileoverview Main component of the application.
- * @author <AlArcoMU>
- */
-
-import { StyleSheet, Text, TextInput, View, Button,FlatList} from 'react-native';
+import { View, TextInput, Button, Text, StyleSheet, FlatList } from 'react-native';
 import { useState } from 'react';
+import GoalInput from './components/GoalInput';
 
-/**
- * Main component of the application.
- */
+
 export default function App() {
-  //Declaramos el hook de estado de componente "newGoal"
-  const [newGoal, setNewGoal] = useState("");
-  const [listGoals, setListGoals] = useState([]);
+  // declaramos el hook de estado de componente "newGoal"
+  const [myGoals, setMyGoals] = useState([]);
 
-  /**
-   * Event handler for when the user enters text into the text input field.
-   * @param {string} enteredText - The text that was entered into the text input field.
-   */
-  function textChangeHandler(enteredText) {
-    setNewGoal(enteredText);
-  }
-
-  /**
-   * Event handler for when the user presses the "Add Goal" button.
-   */
-  function addGoalHandler() {
-    setListGoals(myCurremtGoals => [...myCurremtGoals, newGoal]);
+  function addGoalHandler(newGoalText) {
+    setMyGoals(myCurrentGoals => [...myCurrentGoals,
+    {
+      id: Date.now(),
+      text: newGoalText,
+    }]);
   }
 
   return (
-    <View style={style.container}>
-      <View style={style.InputContainer}>
-        <TextInput
-          onChangeText={textChangeHandler}
-          style={style.TextInput}
-          placeholder='Input your Goal' />
-        <Button
-          title="Add Goal"
-          onPress={addGoalHandler} />
-      </View>
-      <View style={style.goalsContainer}>
+    <View style={styles.container}>
+      <GoalInput onNewGoal={addGoalHandler} />
+
+      <View style={styles.goalsContainer}>
         <FlatList
-        data={listGoals} 
-        renderItem ={
-          ({item}) => (
-            <View style={style.goalCard} key={item}>
-              <Text style={style.goalText}>{item}</Text>
+          data={myGoals}
+          renderItem={(dataItem) => (
+            <View style={styles.goalItem} key={dataItem.item.id}>
+              <Text style={styles.goalText}>{dataItem.item.text}</Text>
             </View>
-          )}
+          )
+          }
         />
       </View>
     </View>
   );
 }
 
-/**
- * StyleSheet for the main component.
- */
-const style = StyleSheet.create({
+const styles = new StyleSheet.create({
   container: {
     flex: 1,
     paddingTop: 70,
-    paddingHorizontal: 20,
+    paddingHorizontal: 15
   },
-  InputContainer: {
-    flex: 1,
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    marginBottom: 20,
-    borderBottomWidth: 2,
-    alignItems: 'center',
-    borderBottomColor: "grey",
-    padding: 4,
-  },
-  TextInput: {
-    borderColor: "grey",
-    borderWidth: 2,
-    padding: 8,
+  textInput: {
+    borderColor: "#CCCCCC",
+    borderWidth: 1,
     width: "70%",
-    height: 40,
+    padding: 10
   },
   goalsContainer: {
-    flex: 4,
+    flex: 5,
   },
-  goalCard:{
-    padding:20,
-    backgroundColor: "#f0554a",
-    marginTop: 10,
-    marginBottom: 10, 
-    borderRadius: 18,
+  goalItem: {
+    padding: 20,
+    backgroundColor: "#092FE0",
+    marginBottom: 10,
+    borderRadius: 12,
   },
-  goalText:{
-    color: "white",
-    fontWeight: "bold",
-    fontSize: 20,
-    textAlign: 'center',
+  goalText: {
+    color: "white"
   }
-});
+
+
+})
+
+
